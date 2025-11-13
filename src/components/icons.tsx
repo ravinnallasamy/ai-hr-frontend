ad0256486282d372dd54a313997b10cfb95ccfeb
Ravin Nallasamy <ravinit001@gmail.com@>
2025-11-14 03:00:29 +0530

    first commit

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 export const SearchIcon = ({ className = 'w-5 h-5' }: { className?: string }) => (
   <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
